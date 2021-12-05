4df4c2ffec4f61fc9cbc9ffe6952347feeca6f43
Eric Martin <eric_martin@me.com>
2021-12-04 22:43:43 -0700

    created signup js

---
+++ public/js/signup.js
@@ -0,0 +1,28 @@
+const signupForm = async function(event) {
+    event.preventDefault;
+
+    const username = document.querySelector('#username-input-signup').value.trim();
+    const password = document.querySelector('#password-input-signup').value.trim();
+
+    console.log(username);
+    console.log(password);
+
+    const response = await fetch('/api/user', {
+        method: 'POST',
+        body: JSON.stringify({
+            username,
+            password
+        }),
+        headers: { 'Content-Type': 'application/json' }
+    });
+
+    if (response.ok) {
+        document.location.replace('/dashboard');
+    } else {
+        alert('Signup Failure!');
+    }
+};
+
+document
+    .querySelector('#signup-form')
+    .addEventListener('submit', signupForm);
