da87f35183563413bc7fcee272b53d6ca59faf0f
Eric Martin <eric_martin@me.com>
2021-12-30 22:29:33 -0700

    updates

--- public/js/comment.js
+++ public/js/comment.js
@@ -1,26 +1,25 @@
-async function commentFormHandler(event) {
+const newFormHandler = async (event) => {
     event.preventDefault();
 
-    const comment_text = document.querySelector('input[name="comment-body"]').value.trim();
-
-    const post_id = window.location.toString().split('/')[
-        window.location.toString().split('/').length - 1
-    ];
+    const comment_text = document.querySelector('#comment-text').value;
 
     if (comment_text) {
-        const response = await fetch('/api/comments', {
+        const response = await fetch(`/api/posts/comment`, {
             method: 'POST',
-            body: JSON.stringify({ post_id, comment_text }),
-            headers: { 'Content-Type': 'application/json' }
+            body: JSON.stringify({ comment_text }),
+            headers: {
+                'Content-Type': 'application/json'
+            },
         });
 
         if (response.ok) {
             document.location.reload();
         } else {
-            alert(response.statusText);
-            document.querySelector('#comment-form').style.display = "block";
+            alert('Failed to create comment');
         }
     }
-}
+};
 
-document.querySelector('.comment-form').addEventListener('submit', commentFormHandler);
+document
+    .querySelector('.new-comment-form')
+    .addEventListener('submit', newFormHandler);
--- public/js/logout.js
+++ public/js/logout.js
@@ -1,16 +1,18 @@
 const logout = async () => {
-    const response = await fetch('/api/users/logout', {
+    const response = await fetch(`/api/users/logout`, {
         method: 'POST',
         headers: {
             'Content-Type': 'application/json'
-        },
+        }
     });
 
     if (response.ok) {
         document.location.replace('/');
     } else {
-        alert('Something went wrong!');
+        alert('Failed to logout');
     }
 };
 
-document.querySelector('#logout').addEventListener('click', logout);
+document
+    .querySelector('#logout')
+    .addEventListener('click', logout);
